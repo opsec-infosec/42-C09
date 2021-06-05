5f106c56d9e0c9fe4d96eea1e7ff8af6d7bf59ad
Dale Furneaux <dfurneau@lab1r1s12.42abudhabi.ae>
2021-06-05 15:39:41 +0400

    Initial Draft

---
+++ ex02.c
@@ -0,0 +1,106 @@
+//
+//  Brought to you by the May Piscene -- DON'T PANIC
+//
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//
+//  Exercise to Include in the test.
+//
+#include "../ex02/ft_split.c"
+
+//
+// Test Function 
+//
+char **ft_split(char *str,char *charset);
+
+//
+// Global Variables -- These have to be global as the seperate threads (main, timer, execute) need to access them
+//
+int func_done = 0;
+int timer_done = 0;
+
+//
+//  Timer Function... We can change the timer by changing the sleep(x) value.. 
+//		The x value is in seconds -- Default to 10 Seconds
+//
+void *timer(void *var) {
+	var = 0x00;
+	sleep(10);
+	timer_done = 1;
+	return (0);
+}
+
+//
+//  Helper Function to print out the split strings
+//
+void output(char **str) {
+	int i = 0;
+	while (*str) {
+		
+		printf("str[%d] = %s\n",i,*str++);
+		i++;
+	}
+	printf("\n");
+}
+
+//
+//  This is the Test Case that we will execution.. If it takes too long, you will have a timeout
+//
+void *execute(void *var) {
+	var = 0x00;
+	
+	char *test_str = "Abu Dhabi 42 May Piscine";
+	char *sep = " ";
+	char *test1_str = "Don't.Panic!";
+	char *sep1 = ".";
+	char *test2_str = "8Time|is9an0illusion-";
+	char *sep2 = "8|90-";
+	
+	char **split;
+
+	printf("\n\nQuote:\nA common mistake that people make when trying to design\nsomething completely foolproof is to underestimate the \ningenuity of complete fools \n\n");
+
+	printf("Test String = %s  | Sep = %s\n",test_str,sep);
+	split = ft_split(test_str,sep);
+	output(split);
+	
+	printf("Test String = %s  | Sep = %s\n",test1_str,sep1);
+	split = ft_split(test1_str,sep1);
+	output(split);
+
+	printf("Test String = %s  | Sep = %s\n",test2_str,sep2);
+	split = ft_split(test2_str,sep2);
+	output(split);
+
+	func_done = 1;
+	return (0);
+}
+
+int	main(void) {
+	pthread_t threadId;  // Execution Thread ID
+	pthread_t timerId;   // Timer Thread ID
+	int	threadId_Sig;
+	int timerID_Sig;
+	
+	// Create 2 threads, timer and execution
+	threadId_Sig = pthread_create(&threadId, NULL, execute,  NULL);  // Create and start the exeuction function
+	timerID_Sig = pthread_create(&timerId, NULL, timer, NULL);   // Create and start the timer function
+
+	while (1) {
+		// if the timer completes before the execution finishes, then we timeout
+		if (timer_done) {
+			pthread_cancel(threadId);
+			printf("*** TIMEOUT ***\n");
+			return (1);
+		}
+		// if execution of the function completes before the timer expires, we stop the timer and exit.
+		if (func_done) {
+			pthread_cancel(timerId);
+			return (0);
+		}
+	}
+	return (0);
+}
